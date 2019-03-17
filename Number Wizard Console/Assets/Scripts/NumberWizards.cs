4d426364e84a2b23831fcd256c81d1a71a449728
Harashen <javierbmetal@gmail.com>
2019-03-17 17:33:22 +0100

    Added starts projects to repository.

---
+++ Number Wizard Console/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizards : MonoBehaviour {
+
+    const int max = 1001;
+    const int min = 1;
+    int guess;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            min = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            max = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return)) {
+            print("I won!");
+            StartGame();
+        }
+	}
+
+    void StartGame() {
+        guess = 500;
+
+        Debug.Log("========================");
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a number in your head, but don't tell me!");
+
+        Debug.Log("The highest number you can pick is " + max);
+        Debug.Log("The lowest number you can pick is "  + min);
+
+        Debug.Log("Is the number higher or lower than " + guess + "?");
+        Debug.Log("Up = higher, down = lower, return = equal");
+    }
+
+    void NextGuess() {
+        guess = (max + min) / 2;
+
+        Debug.Log("Higher or lower than " + guess + "?");
+        Debug.Log("Up = higher, down = lower, return = equal");
+    }
+}
---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+
+    public int maxGuessesAllowed = 10;
+    public Text guessText;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won!");
+            StartGame();
+        }
+	}
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+
+
+        if (--maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
